b3e8974a26e308dd5641bb3920e3f3027ff2b306
mason murray <murray.mason@yahoo.com>
2025-11-26 15:52:38 -0600

    update flask_app

--- flask_app.py
+++ flask_app.py
@@ -4,11 +4,8 @@ import os
 
 app = create_app('ProductionConfig')
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
-
 with app.app_context():
     db.create_all()
     
-app.run()
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
